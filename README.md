f2e1f9adb2dc3e07787398674db314cc67fa2ea7
Hyunwoo Ryu <59816794+Chemical118@users.noreply.github.com>
2022-04-07 01:41:47 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# ASCIrticle Project
+***
+Using Random Forest in citrate synthase activity in heart muscle
+
+***
+ggcheck.py : crawling google or ncbi to get protein data
---
+++ ggcheck.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from fake_useragent import UserAgent
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+import keyboard as k
+import pandas as pd
+
+
+ua = UserAgent()
+userAgent = ua.random
+options = webdriver.ChromeOptions()
+options.add_argument(f'user-agent={userAgent}')
+# options.add_argument('headless')
+# options.add_argument('window-size=1920x1080')
+# options.add_argument("disable-gpu")
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+df = pd.read_excel('data.xls')
+df_list = map(lambda t: t[0], df.values.tolist())
+for i in df_list:
+    driver.get("https://www.ncbi.nlm.nih.gov/protein/?term=" + (i + ' citrate synthase').replace(' ', '+'))
+    k.wait('esc')
